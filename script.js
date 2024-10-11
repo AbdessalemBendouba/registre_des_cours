beacb18d27bce3cf2205e874c8404a0db23bc1a5
Bendouba Abdessalem <moi.cest.salamo@gmail.com>
2024-10-11 23:05:08 +0100

    updating th js file to fit the new aproach of detecting pdf files

--- script.js
+++ script.js
@@ -13,17 +13,15 @@ function toggletheme(){
 
 async function getPdfPaths() {
     try {
-      const response = await fetch('filefinder.php');
-      const txtdata = await response.text();
-      const data = await JSON.parse(txtdata);
+      const response = await fetch('pdf_paths.json');
+      const data = await response.json();
       console.log(data);
-      // Access PDF paths from the data object
       return data;
     } catch (error) {
       console.error('Error fetching PDF data:', error);
       return [];
     }
-  }
+}
 
 // pdf viewer
 
