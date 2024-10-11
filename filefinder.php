12e3a0ad93b8d958e11e81c6065389139213a271
Bendouba Abdessalem <moi.cest.salamo@gmail.com>
2024-10-11 20:59:30 +0100

    creating the file finder and confiduring the results creating the js backbone for it

---
+++ filefinder.php
@@ -0,0 +1,24 @@
+<?php
+
+function getPdfPaths($folderPath) {
+    $pdfPaths = [];
+  
+    if (is_dir($folderPath)) {
+      $files = scandir($folderPath);
+      foreach ($files as $file) {
+        if ($file != '.' && $file != '..' && pathinfo($file, PATHINFO_EXTENSION) === 'pdf') {
+          $pdfPaths[] = $folderPath . '/' . $file;
+        }
+      }
+    }
+  
+    return json_encode($pdfPaths);
+}
+
+$coursesPdfPaths = getPdfPaths('pdf/courses');
+$tdPdfPaths = getPdfPaths('pdf/TD');
+
+header('Content-Type: application/json');
+echo json_encode(['courses' => $coursesPdfPaths, 'TD' => $tdPdfPaths]);
+
+?>
--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let body = document.querySelector("body");
-
 function toggletheme(){
-    if (body.classList.contains('light')){
+    if (body.classList.contains('light')) {
         body.classList.remove('light');
         body.classList.add('dark');
     }else{
@@ -10,8 +9,49 @@ function toggletheme(){
     }
 }
 
-function getFilesFromServer(){
-    var fs = require('fs');
-    var courses_files = fs.readdirSync('/pdf/courses');
-    var TD_files = fs.readdirSync('/pdf/TD');
-}
+// pdf finder handler
+
+async function getPdfPaths() {
+    try {
+      const response = await fetch('filefinder.php');
+      const data = await response.json();
+      // Access PDF paths from the data object
+      return data;
+    } catch (error) {
+      console.error('Error fetching PDF data:', error);
+      return [];
+    }
+  }
+
+// pdf viewer
+
+function showPaths(pdfData) {
+    const CList = document.getElementById('CoursesList');
+    const TList = document.getElementById('TDList');
+        for (const folder in pdfData) {
+            for (const filePath of pdfData[folder]) {
+                const fileName = basename(filePath);
+                const listItem = document.createElement('li');
+                
+                // Set color based on folder
+                if (folder === 'td') {
+                    listItem.className = 'pdf-link td';
+                } else if (folder === 'courses') {
+                    listItem.className = 'pdf-link courses';
+                }
+
+                const link = document.createElement('a');
+                link.href = filePath;
+                link.textContent = fileName;
+            
+                listItem.appendChild(link);
+
+                if (folder === 'td') {
+                    TList.appendChild(listItem);
+                } else if (folder === 'courses') {
+                    CList.appendChild(listItem);
+                }
+  
+            }
+        }
+    }
