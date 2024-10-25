a3decbe0eaed6ae983d075d7464b77dbcc575933
Bendouba Abdessalem <moi.cest.salamo@gmail.com>
2024-10-25 21:55:43 +0100

    adding TP section

--- script.js
+++ script.js
@@ -32,35 +32,86 @@ function basename(path) {
 function showPaths(pdfData) {
     const CList = document.getElementById('CoursesList');
     const TList = document.getElementById('TDList');
+    const PList = document.getElementById('TPList');
     
     for (const folder in pdfData) {
        const files = pdfData[folder];
         for (const filePath of files) {
             const fileName = basename(filePath);
             const listItem = document.createElement('li');
-            
             const link = document.createElement('a');
+            
             link.href = filePath;
             link.textContent = fileName;
             link.target = '_blank';
+
             listItem.appendChild(link);
             
             if (folder.toLowerCase() === 'td') {
                 listItem.className = 'pdf-link td';
-                TList.appendChild(listItem);
+
+                fetchSvgContent('./svg/pdf.svg') // Replace with the actual path
+                .then(svgContent => {
+                  const svgElement = createSvgElement(svgContent);
+                  listItem.appendChild(svgElement);
+                  TList.appendChild(listItem);
+                })
+                .catch(error => {
+                  console.error('Error fetching SVG:', error);
+                });
+
             } else if (folder.toLowerCase() === 'courses') {
                 listItem.className = 'pdf-link courses';
-                CList.appendChild(listItem);
+
+                fetchSvgContent('./svg/pdf.svg') // Replace with the actual path
+                .then(svgContent => {
+                  const svgElement = createSvgElement(svgContent);
+                  listItem.appendChild(svgElement);
+                  CList.appendChild(listItem);
+                })
+                .catch(error => {
+                  console.error('Error fetching SVG:', error);
+                });
+
+            } else if (folder.toLowerCase() === 'tp') {
+                listItem.className = 'pdf-link courses';
+
+                fetchSvgContent('./svg/pdf.svg') // Replace with the actual path
+                .then(svgContent => {
+                  const svgElement = createSvgElement(svgContent);
+                  listItem.appendChild(svgElement);
+                  CList.appendChild(listItem);
+                })
+                .catch(error => {
+                  console.error('Error fetching SVG:', error);
+                });
             }
         }
     }
 }
   
 // Call the functions 
+
+function createSvgElement(svgCode) {
+    const svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svgElement.innerHTML = svgCode;
+    return svgElement;
+}
+
+function fetchSvgContent(svgPath) {
+  return fetch(svgPath)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch SVG');
+      }
+      return response.text();
+    });
+}
+
 getPdfPaths()
     .then(pdfData => {
         showPaths(pdfData);
     })
     .catch(error => {
         console.error('Error fetching PDF data:', error);
-});
+    });
